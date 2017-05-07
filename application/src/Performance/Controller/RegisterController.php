6c51eca7705f338ada7fbfa0d77ca65cf4434950
Alberto Delgado <albert.delgado.2016@salleurl.edu>
2017-05-07 14:25:31 +0200

    upload images S3

--- application/src/Performance/Controller/RegisterController.php
+++ application/src/Performance/Controller/RegisterController.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Performance\Domain\UseCase\SignUp;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Config;
 
 class RegisterController
 {
@@ -40,9 +44,32 @@ class RegisterController
     {
     	$username = $request->request->get('username');
     	$password = $request->request->get('password');
+        $image = $request->files->get('image');
 
     	$this->useCase->execute($username, $password);
+    	$this->uploadImage($image);
+
+
+
 
         return new RedirectResponse($this->url_generator->generate('login'));
+    }
+
+    private function uploadImage($image) {
+        $imageExtension = $image->getClientOriginalExtension();
+        $client = new S3Client([
+            'credentials' => [
+                'key'    => 'AKIAIJOKMDIO2GIE2CIA',
+                'secret' => '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',
+            ],
+            'region' => 'eu-west-1',
+            'version' => 'latest',
+        ]);
+
+        $aws3adapter = new AwsS3Adapter($client, 'mpwarpracticaperformance');
+
+
+        $filesystem = new Filesystem($aws3adapter, new Config([]));
+        $filesystem->write(microtime(true).'.'.$imageExtension, file_get_contents($image->getPathname()));
     }
 }
