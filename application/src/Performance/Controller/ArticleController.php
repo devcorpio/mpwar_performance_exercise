42d82beb4df284a7f650e361711f8863ecc85ece
Alberto Delgado <albert.delgado.2016@salleurl.edu>
2017-05-21 14:45:58 +0200

    ranking redis for auth users

--- application/src/Performance/Controller/ArticleController.php
+++ application/src/Performance/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace Performance\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Performance\Domain\UseCase\ReadArticle;
 use Predis\Client as PredisClient;
 
@@ -24,10 +25,16 @@ class ArticleController
      */
     private $cache;
 
-    public function __construct(\Twig_Environment $templating, ReadArticle $useCase, PredisClient $cache) {
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(\Twig_Environment $templating, ReadArticle $useCase, PredisClient $cache, SessionInterface $session) {
         $this->template = $templating;
         $this->useCase = $useCase;
         $this->cache = $cache;
+        $this->session = $session;
     }
 
     public function get($article_id)
@@ -45,6 +52,12 @@ class ArticleController
         }
 
         $this->cache->zincrby('globalranking', 1, serialize($article));
+
+        if ($this->session->get('author_id')) {
+            $this->cache->zincrby('userranking_'.$this->session->get('author_id'), 1, serialize($article));
+        }
+
+
 
         return new Response($this->template->render('article.twig', ['article' => $article]));
     }
--- application/src/Performance/Controller/HomeController.php
+++ application/src/Performance/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace Performance\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Performance\Domain\UseCase\ListArticles;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Predis\Client as PredisClient;
 
 class HomeController
@@ -23,20 +24,32 @@ class HomeController
      */
     private $cache;
 
-    public function __construct(\Twig_Environment $templating, ListArticles $useCase, PredisClient $cache) {
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(\Twig_Environment $templating, ListArticles $useCase, PredisClient $cache, SessionInterface $session) {
         $this->template = $templating;
         $this->useCase = $useCase;
         $this->cache = $cache;
+        $this->session = $session;
     }
 
     public function get()
     {
         $articles = $this->useCase->execute();
         $articlesGlobalTopRanking = $this->cache->zrevrange('globalranking', 0, 4);
+        $articlesAuthTopRanking = [];
+
+        if ($this->session->get('author_id')) {
+            $articlesAuthTopRanking = $this->cache->zrevrange('userranking_'.$this->session->get('author_id'), 0, 4);
+        }
 
         return new Response($this->template->render('home.twig', [
             'articles' => $articles,
-            'articlesGlobalRanking' => $this->unserializeRanking($articlesGlobalTopRanking)
+            'articlesGlobalRanking' => $this->unserializeRanking($articlesGlobalTopRanking),
+            'articlesAuthRanking' => $this->unserializeRanking($articlesAuthTopRanking)
         ]));
     }
 
